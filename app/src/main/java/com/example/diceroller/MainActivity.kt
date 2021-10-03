ea56b0a3fb994140f6c9ed3cd3fb5679075d89f3
Thalisson <thalissonmt@gmail.com>
2021-10-03 16:42:53 -0300

    adding vectorDrawables and finishing the app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -21,6 +20,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6) + 1
-        binding.resultText.text = randomInt.toString()
+        val drawableResources = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImage.setImageResource(drawableResources)
     }
 }
